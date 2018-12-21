b6e241027dcc4331c9d60fd3e5bc0e9d1c7e23da
redab <redabs@users.noreply.github.com>
2018-12-21 17:10:29 +0100

    Can now remove employee language, API change

--- src/sample/DbWrapper.java
+++ src/sample/DbWrapper.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.sql.*;
 
 public class DbWrapper {
@@ -7,8 +9,8 @@ public class DbWrapper {
     
     private String URL = "jdbc:mysql://localhost:3306/museum";
     private String driver = "com.mysql.cj.jdbc.Driver";
-    private String userID = "root";
-    private String password = "";
+    private String userID = "iv1351";
+    private String password = "1234";
     private Statement statement;
     
     public void connect() {
@@ -17,32 +19,37 @@ public class DbWrapper {
             connection = DriverManager.getConnection(URL, userID, password);
             connection.setAutoCommit(false);
             System.out.println("Connected to " + URL + " using " + driver);
+            statement = connection.createStatement();
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
     
-    public void printAllGuides() throws Exception {
+    public ArrayList<String> getAllGuides() throws Exception {
+        ArrayList<String> guides = new ArrayList<String>();
         ResultSet result = statement.executeQuery("select Name from Employee where PersonNr in (select PersonNr from GuidedTour)");
         while(result.next()) {
-            System.out.println("Name: " + result.getString("Name"));
+            guides.add(result.getString("Name"));
         }
+        return guides;
     }
     
-    public void printAllGuideLanguages(String guideName) throws Exception {
+    public ArrayList<String> getAllGuideLanguages(String guideName) throws Exception {
+        ArrayList<String> guideLanguages = new ArrayList<String>();
         ResultSet personNrResult = statement.executeQuery("select PersonNr from Employee where Name='"+guideName+"'");
         if(personNrResult.next()) {
             String query = "select LanguageName from EmployeeLanguage Where PersonNr='" + personNrResult.getString("PersonNr") + "'";
             ResultSet result = statement.executeQuery(query);
             while(result.next()) {
-                System.out.println(result.getString("LanguageName"));
+                guideLanguages.add(result.getString("LanguageName"));
             }
         } else {
             System.out.println("No employee with the name " + guideName);
         }
+        return guideLanguages;
     }
     
-    public void addEmployeeLanguage(String employeeName, String languageName) throws Exception {
+    public boolean addEmployeeLanguage(String employeeName, String languageName) throws Exception {
         ResultSet r = statement.executeQuery("select Name from Language where Name='" + languageName + "'");
         if(r.next()) {
             ResultSet personNrResult = statement.executeQuery("select PersonNr from Employee where Name='" + employeeName + "'");
@@ -52,6 +59,7 @@ public class DbWrapper {
                     statement.executeUpdate("insert into EmployeeLanguage values(" + personNr + ", '" + languageName + "')");
                     connection.commit();
                     System.out.println(languageName + " added for employee " + employeeName);
+                    return true;
                 } catch (SQLIntegrityConstraintViolationException e) {
                     System.out.println(languageName + " already exists for " + employeeName);
                 }
@@ -61,27 +69,47 @@ public class DbWrapper {
         } else {
             System.out.println("No language with the name " + languageName);
         }
+
+        return false;
     }
     
-    public void createTheFuckingStatementFromANonStaticContext() throws Exception {
-        statement = connection.createStatement();
+    public boolean removeEmployeeLanguage(String employeeName, String languageName) throws Exception {
+        ResultSet personNrResult = statement.executeQuery("select PersonNr from Employee where Name='" + employeeName + "'");
+        if(personNrResult.next()) {
+            String personNr = personNrResult.getString("PersonNr");
+            ResultSet foo = statement.executeQuery("select * from GuidedTour where PersonNr='" + personNr + "' and LanguageName='" + languageName + "'");
+            if(!foo.next()) {
+                statement.executeUpdate("delete from EmployeeLanguage where PersonNr in (select PersonNr from Employee where Name='" + employeeName + "') and LanguageName='" + languageName + "'");
+                connection.commit();
+                return true;
+            } else {
+				System.out.println("There exists a guided tour held by this guide in the language which is to be deleted. Language was not deleted.");
+            }
+        } else {
+            System.out.println("No employee with the name " + employeeName);
+        }
+        return false;
     }
-    
+
     public static void main(String[] args) {
         DbWrapper wrapper = new DbWrapper();
         wrapper.connect();
         try {
-            wrapper.createTheFuckingStatementFromANonStaticContext();
-            System.out.println("All Guides: ");
-            wrapper.printAllGuides();
-            System.out.println();
+            for(String i : wrapper.getAllGuides()) {
+                System.out.println(i);
+            }
             
-            System.out.println("All Guide Languages for Ghost Face");
-            wrapper.printAllGuideLanguages("John Baptiste");
-            System.out.println();
-            
-            System.out.println("Insert Language: ");
-            wrapper.addEmployeeLanguage("Ghost Face", "Norwegian");
+            for(String i : wrapper.getAllGuideLanguages("John Baptiste")) {
+                System.out.println(i);
+            }
+
+            if(wrapper.addEmployeeLanguage("Ghost Face", "Norwegian")) {
+                // Success
+			}
+
+            if(wrapper.removeEmployeeLanguage("Ghost Face", "Polish")) {
+				// Success
+			}
         } catch(Exception e) {
             e.printStackTrace();
         }
